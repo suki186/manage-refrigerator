e658786bb079f35a8fe36cb60b34a63a24020f0b
Suki <tues1205@naver.com>
2025-05-26 11:02:54 +0900

    Initial Commit

---
+++ manageRefrigerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  manageRefrigerator
+//
+//  Created by suki on 5/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
