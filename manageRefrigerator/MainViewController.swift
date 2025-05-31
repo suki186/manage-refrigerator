9fecb8ee37e5bd70bf8563a5ef040120d95e90ed
Suki <tues1205@naver.com>
2025-05-31 19:29:46 +0900

    💄 : 메인페이지 버튼 배치
    메인페이지 버튼 배치

---
+++ manageRefrigerator/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  manageRefrigerator
+//
+//  Created by suki on 5/31/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
