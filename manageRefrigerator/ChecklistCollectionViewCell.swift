60d2ddb1e11713b2fa7390efa02aef5da1879bbc
Suki <tues1205@naver.com>
2025-06-04 22:37:31 +0900

    💄 Design: 카테고리 cell

---
+++ manageRefrigerator/ChecklistCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ChecklistCollectionViewCell.swift
+//  manageRefrigerator
+//
+//  Created by suki on 6/4/25.
+//
+
+import UIKit
+
+class ChecklistCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var checkText: UITextField!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
