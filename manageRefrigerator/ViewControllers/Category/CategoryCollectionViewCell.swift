b5466c43452c8759cff9301418f7bad016c2304a
Suki <tues1205@naver.com>
2025-06-11 01:33:46 +0900

    ✨ Feat: 재료 등록하기

--- manageRefrigerator/ViewControllers/Category/CategoryCollectionViewCell.swift
+++ manageRefrigerator/ViewControllers/Category/CategoryCollectionViewCell.swift
@@ -19,7 +19,22 @@ class CategoryCollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+    }
+    
+    // 선택 효과
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                contentView.layer.borderColor = UIColor(red: 98/255, green: 209/255, blue: 239/255, alpha: 1.0).cgColor
+                contentView.layer.borderWidth = 2
+            } else {
+                contentView.layer.borderColor = UIColor.clear.cgColor
+                contentView.layer.borderWidth = 0
+            }
+        }
     }
 
 }
